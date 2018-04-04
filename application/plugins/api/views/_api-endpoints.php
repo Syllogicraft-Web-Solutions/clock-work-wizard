2d516ca72fb40e92f80acd1063c5eacd63d0cb86
John Abelardo Manangan II <abelardomanangan@gmail.com>
2018-04-05 03:31:11 +0800

    Added a more detailed json message responses

--- application/plugins/api/views/_api-endpoints.php
+++ application/plugins/api/views/_api-endpoints.php
@@ -35,16 +35,16 @@ if ($endpoint == 'clocker' && $action == 'verify-clocker-action' && (isset($_GET
                               echo json_encode(['code' => 200, 'message' => 'Successful', 'responseText' => true]);
                               return;
                          } else {
-                              echo json_encode(['code' => 403, 'message' => 'Authentication failed. Please try logging in again.']);
+                              echo json_encode(['code' => 403, 'message' => 'Unable to complete action. Please make sure you are scanning the code of your manager.']);
                               return;
                          }
                     } else {
-                         echo json_encode(['code' => 403, 'message' => 'Authentication failed. Please try logging in again.']);
+                         echo json_encode(['code' => 403, 'message' => 'Code doesn\'t exists. Please try logging in again.']);
                          return;
                     }
                }
           } else
-               echo json_encode(['code' => 403, 'message' => 'Authentication failed. Please try logging in again.']);
+               echo json_encode(['code' => 403, 'message' => 'Auth failed: No token provided. Please try logging in again.']);
      } else
           echo json_encode(['code' => 301, 'message' => 'Unable to complete action.']);
 }
